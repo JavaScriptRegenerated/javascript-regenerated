1e0b5e5ff09a30b3da0cba359c2f4062b92f2145
Patrick Smith <pgwsmith@gmail.com>
2021-07-25 10:45:56 +1000

    Add sending functions slide

--- app/navs/primary.tsx
+++ app/navs/primary.tsx
@@ -3,16 +3,16 @@ import { Link } from "remix";
 export const makeNavItems = (
   <>
     <li>
-      <Link to="/make/html-renderer">{"HTML renderer"}</Link>
+      <a href="/make/html-renderer">{"HTML renderer"}</a>
     </li>
     <li>
-      <Link to="/make/schema">{"Schema validator"}</Link>
+      <a href="/make/schema">{"Schema validator"}</a>
     </li>
     <li>
-      <Link to="/make/parser">{"Parser"}</Link>
+      <a href="/make/parser">{"Parser"}</a>
     </li>
     <li>
-      <Link to="/make/multiproducers">{"Multicolored components"}</Link>
+      <a href="/make/multiproducers">{"Multicolored components"}</a>
     </li>
   </>
 );
@@ -22,56 +22,51 @@ export function PrimaryNavigation() {
     <nav>
       <ul>
         <li>
-          <Link to="/1-linear">{"Linear thinking"}</Link>
+          <a href="/1-linear">{"Linear thinking"}</a>
         </li>
         <li>
-          <Link to="/1-objects">{"Object-oriented"}</Link>
+          <a href="/1-objects">{"Object-oriented"}</a>
         </li>
         <li>
-          <Link to="/1-what-not-how">{"What, not how"}</Link>
+          <a href="/1-what-not-how">{"What, not how"}</a>
         </li>
         <li>
-          <Link to="/1-names">{"Two hardest things"}</Link>
+          <a href="/1-names">{"Two hardest things"}</a>
         </li>
         <li>
-          <Link to="/1-components">{"Component mindset"}</Link>
+          <a href="/1-components">{"Component mindset"}</a>
         </li>
         <li role="separator" data-y="1" />
         <li>
-          <Link to="/2-methods-vs-messages">{"Methods vs messages"}</Link>
+          <a href="/2-methods-vs-messages">{"Methods vs messages"}</a>
         </li>
         <li>
-          <Link to="/2-message-generators">{"Message generators"}</Link>
+          <a href="/2-message-generators">{"Message generators"}</a>
         </li>
         <li>
-          <Link to="/3-message-processors">{"Message processors"}</Link>
+          <a href="/3-message-processors">{"Message processors"}</a>
         </li>
         <li>
-          <Link to="/3-functions">{"Sending functions"}</Link>
-        </li>
-        <li>
-          <Link to="/generator-functions-vs-classes">
-            {"Generator functions vs classes"}
-          </Link>
+          <a href="/3-sending-functions">{"Sending functions"}</a>
         </li>
         <li role="separator" data-y="1" />
         {makeNavItems}
         <li role="separator" data-y="1" />
         <li>
-          <Link to="/4-function-of">{"More than the view"}</Link>
+          <a href="/4-function-of">{"More than the view"}</a>
         </li>
         <li>
-          <Link to="/0-javascript-everywhere">{"JavaScript is everywhere"}</Link>
+          <a href="/4-javascript-everywhere">{"JavaScript is everywhere"}</a>
         </li>
         <li>
-          <Link to="/4-benefits">{"Unique benefits"}</Link>
+          <a href="/4-benefits">{"Unique benefits"}</a>
         </li>
         <li>
-          <Link to="/libraries">{"Open source libraries"}</Link>
+          <a href="/libraries">{"Open source libraries"}</a>
         </li>
         <li role="separator" data-y="1" />
         <li>
-          <Link to="/thanks">{"Thanks"}</Link>
+          <a href="/thanks">{"Thanks"}</a>
         </li>
       </ul>
     </nav>
--- app/routes/3-sending-functions.tsx
+++ app/routes/3-sending-functions.tsx
@@ -178,11 +178,87 @@ function MessageProcessorsGraphic(): JSX.Element {
   );
 }
 
+function SendingFunctionsCode() {
+  return (
+    <>
+      <h2>Parser component</h2>
+      <CodeBlock language="javascript" smaller>
+        {`
+import { parse, mustEnd } from 'yieldparser';
+
+function* IPAddress() {
+  const firstString = yield /^\d+/;
+  const firstDigit = parseInt(firstString, 10);
+  yield '.';
+  const secondString = yield /^\d+/;
+  const secondDigit = parseInt(secondString, 10);
+  yield '.';
+  const thirdString = yield /^\d+/;
+  const thirdDigit = parseInt(thirdString, 10);
+  yield '.';
+  const fourthString = yield /^\d+/;
+  const fourthDigit = parseInt(fourthString, 10);
+  yield mustEnd;
+  return [firstDigit, secondDigit, thirdDigit, fourthDigit];
+}
+
+parse('1.2.3.4', IPAddress());
+/*
+{
+  success: true,
+  result: [1, 2, 3, 4],
+  remaining: '',
+}
+*/
+`.trim()}
+      </CodeBlock>
+      <h2>Nested parser components</h2>
+      <CodeBlock language="javascript" smaller>
+        {`
+import { parse, mustEnd } from 'yieldparser';
+
+function* Digit() {
+  const [digit] = yield /^\d+/;
+  const value = parseInt(digit, 10);
+  // Validate digit is inclusively between 0 and 255.
+  if (value < 0 || value > 255) {
+    return new Error(\`\${value} must be >= 0 && <= 255\`);
+  }
+  // The number value is the result from this component.
+  return value;
+}
+
+function* IPAddress() {
+  const first = yield Digit;
+  yield '.';
+  const second = yield Digit;
+  yield '.';
+  const third = yield Digit;
+  yield '.';
+  const fourth = yield Digit;
+  yield mustEnd;
+  return [first, second, third, fourth];
+}
+
+parse('1.2.3.4', IPAddress());
+/*
+{
+  success: true,
+  result: [1, 2, 3, 4],
+  remaining: '',
+}
+*/
+`.trim()}
+      </CodeBlock>
+    </>
+  );
+}
+
 export default function MessageProcessorsPage() {
   return (
     <main data-measure="center" data-text="center">
-      <h1>Message Processors</h1>
-      <MessageProcessorsGraphic />
+      <h1>Functions can be messages too</h1>
+      <SendingFunctionsCode />
 
       {/* <hr data-y="100vh" /> */}
     </main>
