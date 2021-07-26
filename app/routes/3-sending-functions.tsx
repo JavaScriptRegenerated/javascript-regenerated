0dfbda7256780aad1fff8112f49e99366d878d25
Patrick Smith <pgwsmith@gmail.com>
2021-07-26 13:51:38 +1000

    Fix mistake

--- app/routes/3-sending-functions.tsx
+++ app/routes/3-sending-functions.tsx
@@ -18,20 +18,7 @@ export let links: LinksFunction = () => {
 };
 
 const simpleIcons = [] as const;
-const heroIcons = [
-  
-  "solid/reply.svg",
-  "solid/film.svg",
-  "solid/play.svg",
-  "solid/document.svg",
-  "solid/document-text.svg",
-  "solid/check-circle.svg",
-  "solid/light-bulb.svg",
-  "solid/thumb-up.svg",
-  "solid/fast-forward.svg",
-  "solid/search-circle.svg",
-  "solid/gift.svg",
-] as const;
+const heroIcons = [] as const;
 
 export let loader: LoaderFunction = async () => {
   return {
@@ -42,142 +29,6 @@ export let loader: LoaderFunction = async () => {
   };
 };
 
-const LoadedIcon = typeLoadedIconComponent<
-  typeof simpleIcons[-1] | typeof heroIcons[-1]
->();
-
-const textIndent = 30;
-const textY = (index: number) => index * 20 + 20;
-const iconY = (index: number) => index * 20 + 12;
-
-function MessageProcessorsGraphic(): JSX.Element {
-  return (
-    <svg viewBox="0 0 180 220">
-      <rect width="100%" height="100%" fill="black" />
-      <g x="20" fontSize="4" fill="white" letterSpacing={0.05}>
-        <LoadedIcon
-          name="solid/film.svg"
-          width={12}
-          height={12}
-          x={10}
-          y={iconY(0)}
-        />
-        <text x={textIndent} y={textY(0)}>
-          <tspan fontWeight="bold">They accept a generator function</tspan>
-        </text>
-
-        <LoadedIcon
-          name="solid/document.svg"
-          width={12}
-          height={12}
-          x={10}
-          y={iconY(1)}
-        />
-        <text x={textIndent} y={textY(1)}>
-          <tspan fontWeight="bold">They initialise some sort of state</tspan>
-        </text>
-
-        <LoadedIcon
-          name="solid/play.svg"
-          width={12}
-          height={12}
-          x={10}
-          y={iconY(2)}
-        />
-        <text x={textIndent} y={textY(2)}>
-          <tspan fontWeight="bold">They iterate though the generator, receiving messages</tspan>
-        </text>
-
-        <LoadedIcon
-          name="solid/search-circle.svg"
-          width={12}
-          height={12}
-          x={10}
-          y={iconY(3)}
-        />
-        <text x={textIndent} y={textY(3)}>
-          <tspan fontWeight="bold">They check if they recognise the message</tspan>
-        </text>
-
-        <LoadedIcon
-          name="solid/fast-forward.svg"
-          width={12}
-          height={12}
-          x={10}
-          y={iconY(4)}
-        />
-        <text x={textIndent} y={textY(4)}>
-          <tspan fontWeight="bold">Unrecognised messages are ignored, so skip to next message</tspan>
-        </text>
-
-        <LoadedIcon
-          name="solid/light-bulb.svg"
-          width={12}
-          height={12}
-          x={10}
-          y={iconY(5)}
-        />
-        <text x={textIndent} y={textY(5)}>
-          <tspan fontWeight="bold">Recognised messages are processed</tspan>
-        </text>
-        <text x={textIndent} y={textY(5) + 8}>
-          <tspan>e.g. Match regex, add number, append string, fetch URL.</tspan>
-        </text>
-
-        <LoadedIcon
-          name="solid/document-text.svg"
-          width={12}
-          height={12}
-          x={10}
-          y={iconY(6)}
-        />
-        <text x={textIndent} y={textY(6)}>
-          <tspan fontWeight="bold">The state might change due to processing the message</tspan>
-        </text>
-        <text x={textIndent} y={textY(6) + 8}>
-          <tspan>e.g. String was appended, number changed.</tspan>
-        </text>
-
-        <LoadedIcon
-          name="solid/reply.svg"
-          width={12}
-          height={12}
-          x={10}
-          y={iconY(7)}
-        />
-        <text x={textIndent} y={textY(7)}>
-          <tspan fontWeight="bold">Reply to messages that expect one</tspan>
-        </text>
-        <text x={textIndent} y={textY(7) + 8}>
-          <tspan>e.g. Return regex matches or fetch reponse.</tspan>
-        </text>
-
-        <LoadedIcon
-          name="solid/play.svg"
-          width={12}
-          height={12}
-          x={10}
-          y={iconY(8)}
-        />
-        <text x={textIndent} y={textY(8)}>
-          <tspan fontWeight="bold">They keep iterating though the generator until it’s done</tspan>
-        </text>
-
-        <LoadedIcon
-          name="solid/check-circle.svg"
-          width={12}
-          height={12}
-          x={10}
-          y={iconY(9)}
-        />
-        <text x={textIndent} y={textY(9)}>
-          <tspan fontWeight="bold">They might return the state or some sort of result</tspan>
-        </text>
-      </g>
-    </svg>
-  );
-}
-
 function SendingFunctionsCode() {
   return (
     <>
@@ -187,16 +38,16 @@ function SendingFunctionsCode() {
 import { parse, mustEnd } from 'yieldparser';
 
 function* IPAddress() {
-  const firstString = yield /^\d+/;
+  const [firstString] = yield /^\d+/;
   const firstDigit = parseInt(firstString, 10);
   yield '.';
-  const secondString = yield /^\d+/;
+  const [secondString] = yield /^\d+/;
   const secondDigit = parseInt(secondString, 10);
   yield '.';
-  const thirdString = yield /^\d+/;
+  const [thirdString] = yield /^\d+/;
   const thirdDigit = parseInt(thirdString, 10);
   yield '.';
-  const fourthString = yield /^\d+/;
+  const [fourthString] = yield /^\d+/;
   const fourthDigit = parseInt(fourthString, 10);
   yield mustEnd;
   return [firstDigit, secondDigit, thirdDigit, fourthDigit];
