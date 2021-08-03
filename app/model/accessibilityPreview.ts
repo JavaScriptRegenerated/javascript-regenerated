9b00f47fea612c0982b4974d0ff12011d0db923b
Patrick Smith <pgwsmith@gmail.com>
2021-08-04 08:56:38 +1000

    Add screen reader previews

---
+++ app/model/accessibilityPreview.ts
@@ -0,0 +1,136 @@
+const labelledTextInput: unique symbol = Symbol("labelledTextInput");
+const button: unique symbol = Symbol("button");
+const checkbox: unique symbol = Symbol("checkbox");
+const fieldset: unique symbol = Symbol("fieldset");
+export const Disabled: unique symbol = Symbol("Disabled");
+
+export const ids = {
+  labelledTextInput,
+  button,
+  fieldset,
+} as const;
+
+export function LabelledTextInput(
+  name: string,
+  options: Array<typeof Disabled> = []
+) {
+  return Object.freeze({
+    type: ids.labelledTextInput,
+    name,
+    disabled: options.includes(Disabled),
+  } as const);
+}
+
+export function Button(name: string, options: Array<typeof Disabled> = []) {
+  return Object.freeze({
+    type: ids.button,
+    name,
+    disabled: options.includes(Disabled),
+  } as const);
+}
+
+export function Fieldset(
+  legend: string,
+  children: Array<ReturnType<typeof LabelledTextInput | typeof Button>>,
+  options: Array<typeof Disabled> = []
+) {
+  return Object.freeze({
+    type: ids.fieldset,
+    name: legend,
+    children,
+    disabled: options.includes(Disabled),
+  } as const);
+}
+
+export type RenderingMessage = ReturnType<
+  typeof LabelledTextInput | typeof Button | typeof Fieldset
+>;
+
+export function* processHTML(
+  generator: () => Iterable<RenderingMessage>
+): Generator<string> {
+  for (const message of generator()) {
+    if (message.type === ids.labelledTextInput) {
+      yield "<label class='Y'>";
+      yield `<span>${message.name}</span>`;
+      yield "<input type=text";
+      if (message.disabled) yield " disabled";
+      yield ">";
+      yield "</label>";
+      yield "\n";
+    } else if (message.type === ids.button) {
+      yield "<button>";
+      if (message.disabled) yield " disabled";
+      yield message.name;
+      yield "</button>";
+      yield "\n";
+    } else if (message.type === ids.fieldset) {
+      yield "<fieldset";
+      if (message.disabled) yield " disabled";
+      yield ">";
+      yield "<legend>";
+      yield message.name;
+      yield "</legend>";
+      yield* processHTML(() => message.children);
+      yield "\n";
+    }
+  }
+}
+
+export function* previewVoiceOver(generator: () => Iterable<RenderingMessage>) {
+  for (const message of generator()) {
+    if (message.type === ids.labelledTextInput) {
+      if (message.disabled) {
+        yield `${message.name}. Dimmed. Edit text.`;
+      } else {
+        yield `${message.name}. Edit. Type in text.`;
+      }
+    } else if (message.type === ids.button) {
+      if (message.disabled) {
+        yield `${message.name}. Dimmed. Edit text.`;
+      } else {
+        yield `${message.name}. Button.`;
+      }
+    } else if (message.type === ids.fieldset) {
+      if (message.disabled) {
+        yield `${message.name}. Dimmed. Group.`;
+      } else {
+        const child = message.children[0];
+        if (child !== undefined) {
+          yield* previewJAWS(() => [child]);
+        }
+        yield `${message.name}. Group.`;
+      }
+    }
+  }
+}
+
+export function* previewJAWS(
+  generator: () => Iterable<RenderingMessage>
+): Generator<string> {
+  for (const message of generator()) {
+    if (message.type === ids.labelledTextInput) {
+      if (message.disabled) {
+        yield `[Label] ${message.name}. [Input] Edit. Unavailable.`;
+      } else {
+        yield `${message.name}. Edit text.`;
+      }
+    } else if (message.type === ids.button) {
+      if (message.disabled) {
+        yield `${message.name}. Button. Unavailable.`;
+      } else {
+        yield `${message.name}. Button.`;
+      }
+    } else if (message.type === ids.fieldset) {
+      if (message.disabled) {
+        yield `Group. Start. ${message.name}.`;
+      } else {
+        yield `${message.name}. Group.`;
+        const child = message.children[0];
+        if (child !== undefined) {
+          yield* previewJAWS(() => [child]);
+        }
+      }
+    }
+  }
+}
---
+++ app/routes/make/accessibility-preview.tsx
@@ -0,0 +1,69 @@
+import type { MetaFunction, LinksFunction, LoaderFunction } from "remix";
+import { useRouteData } from "remix";
+import {
+    Disabled,
+  LabelledTextInput,
+  previewJAWS,
+  previewVoiceOver,
+  processHTML,
+} from "../../model/accessibilityPreview";
+import { Await } from "../../types/helpers";
+import { CodeBlock } from "../../view/code";
+import { formatJavaScript } from "../../view/codeFormatting";
+import { NamedSection } from "../../view/semantics";
+
+export let meta: MetaFunction = () => {
+  return {
+    title: "Renderers: JavaScript Regenerated",
+    description: "One components that renders to many targets",
+  };
+};
+
+export let links: LinksFunction = () => {
+  return [];
+};
+
+function* HTMLComponent() {
+  yield LabelledTextInput("First name");
+  yield LabelledTextInput("Last name", [Disabled]);
+}
+
+export async function loader(args: Parameters<LoaderFunction>[0]) {
+  return {
+    functionsSource: {
+      HTMLComponent: formatJavaScript(HTMLComponent.toString()),
+    },
+  };
+}
+
+export default function MakeRenderer() {
+  let data: Await<ReturnType<typeof loader>> = useRouteData();
+
+  const outputHTML = Array.from(processHTML(HTMLComponent)).join("");
+  const outputJAWS = Array.from(previewJAWS(HTMLComponent)).join("\n");
+  const outputVoiceOver = Array.from(previewVoiceOver(HTMLComponent)).join("\n");
+
+  return (
+    <main data-measure="center">
+      <h1>{"Accessibility Tools Preview"}</h1>
+
+      <NamedSection id="example-1" heading={<h2>Form Component</h2>}>
+        <CodeBlock language="javascript">
+          {data.functionsSource.HTMLComponent}
+        </CodeBlock>
+        <CodeBlock language="html">{outputHTML}</CodeBlock>
+        <output style={{ border: "1px solid black", padding: "1rem" }}>
+          <div dangerouslySetInnerHTML={{ __html: outputHTML }}></div>
+        </output>
+        <h3>JAWS</h3>
+        <pre>{outputJAWS}</pre>
+        <h3>VoiceOver</h3>
+        <pre>{outputVoiceOver}</pre>
+      </NamedSection>
+
+      <NamedSection id="jaws-processor" heading={<h2>JAWS Processor</h2>}>
+        <CodeBlock language="javascript">{previewJAWS.toString()}</CodeBlock>
+      </NamedSection>
+    </main>
+  );
+}
